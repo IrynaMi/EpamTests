02cfd56abffef03bafd0ad0d5844c870b21bb6d5
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-04 10:52:30 +0300

    Task 2 (corrected)

--- Task2.java
+++ Task2.java
@@ -3,7 +3,7 @@
 public class Task2 {
 	public static void main(String[] args) {
 		Task2.weightToTons(234);
-		Task2.weightToGramm(234);
+		Task2.weightToGrams(234);
 		Task2.weightToMg(234);
 		Task2.ringSquare(40, 32);
 		Task2.changeVar(24, 18);
@@ -13,22 +13,25 @@ public class Task2 {
 		Task2.reverseNumber(1234567);
 	}
 	
-	static int weightToTons(int mKg) {
-		int mT = mKg / 1000;
-		System.out.println(mKg + " Kgs = " + mT + " Tonns");
-		return mT;
+	static double weightToTons(int mKg) {
+		final double KG_TO_TONS = 0.001;
+		double mTons = mKg * KG_TO_TONS;
+		System.out.println(mKg + " Kgs = " + mTons + " Tonns");
+		return mTons;
 	}
 	
-	static int weightToGramm(int mKg) {
-		int mGr = mKg * 1000;
-		System.out.println(mKg + " Kgs = " + mGr + " Gramms");
-		return mGr;
+	static int weightToGrams(int mKg) {
+		final int KG_TO_GRAMS = 1000;
+		int mGrams = mKg * KG_TO_GRAMS ;
+		System.out.println(mKg + " Kgs = " + mGrams + " Gramms");
+		return mGrams;
 	}
 	
 	static int weightToMg(int mKg) {
-		int mGr = mKg * 1000000;
-		System.out.println(mKg + " Kgs = " + mGr + " MilliGramms");
-		return mGr;
+		final int KG_TO_MGRAMS = 1_000_000;
+		int mGrams = mKg * KG_TO_MGRAMS;
+		System.out.println(mKg + " Kgs = " + mGrams + " MiliGramms");
+		return mGrams;
 	}
 	
 	static double ringSquare (double r1, double r2) {
@@ -38,33 +41,27 @@ public class Task2 {
 	}
 	
 	static void changeVar (int a, int b) {
-		a = a+b;
+		a = a + b;
 		b = a - b;
-		a = a-b;
+		a = a - b;
 		System.out.println("a = " + a + ", b = "+ b);
 	}
 	
-	static void isNumberAscending (int n) {
+	static boolean isNumberAscending (int n) {
 		boolean result = false;
 		int th = n/1000;
 		int hund = (n/100)%10;
 		int doz = (n/10)%10;
 		int units = n%10;
-		System.out.println(th + " "+ hund+ " "+ doz+ " "+  units);
 		if (th < hund) {
 			if (hund < doz) {
 				if (doz < units){
 					result = true;
-					System.out.println(result);
-				} else {
-					System.out.println(result);
-				}
-			} else {
-				System.out.println(result);
+				} 
 			}
-		}else {
-			System.out.println(result);
 		}
+		System.out.println(result);
+		return result;
 	}
 	
 	static long multiplicationOfNum(int n) {
@@ -90,28 +87,30 @@ public class Task2 {
 	}
 	
 	static double averageArifmetical (int n) {
+		final int DIGITS_OF_NUMBER = 6;
 		int hundTh = n/100000;
 		int tenTh = (n/10000)%10;
 		int th = (n/1000)%10;
 		int hund = (n/100)%10;
 		int doz = (n/10)%10;
 		int units = n%10;
-		double ar = (hundTh*tenTh*th*hund*doz*units)/6;
-		System.out.println("Average arithmetical of " + n + " = "+ ar);
-		return ar;
+		double averageArithmetical = (hundTh*tenTh*th*hund*doz*units)/DIGITS_OF_NUMBER;
+		System.out.println("Average arithmetical of " + n + " = "+ averageArithmetical);
+		return averageArithmetical;
 		
 	}
 	
 	static double averageGeometical (int n) {
+		final double DIGITS_OF_NUMBER = 6.;
 		int hundTh = n/100000;
 		int tenTh = (n/10000)%10;
 		int th = (n/1000)%10;
 		int hund = (n/100)%10;
 		int doz = (n/10)%10;
 		int units = n%10;
-		double gm = Math.pow(hundTh*tenTh*th*hund*doz*units, 1/6.);
-		System.out.println("Average geometrical of " + n + " = "+ gm);
-		return gm;
+		double averageGeometrical = Math.pow(hundTh*tenTh*th*hund*doz*units, 1/DIGITS_OF_NUMBER);
+		System.out.println("Average geometrical of " + n + " = "+ averageGeometrical);
+		return averageGeometrical;
 	}
 	
 	static int reverseNumber (int n) {

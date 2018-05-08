9f74b9597e0e7ffd650382e0a1b5337e71901011
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-08 23:52:30 +0300

    Task 3 (исправлено)

--- Task3.java
+++ Task3.java
@@ -1,6 +1,10 @@
+package javaCoreTests;
+
 
 
 import java.util.Random;
+
+import javax.naming.NamingEnumeration;
 
 public class Task3 {
 	
@@ -9,154 +13,115 @@ public class Task3 {
 		Task3.calcHeads(100);
 		Task3.calcEyes(100);
 		Task3.isVowel1('f');
-		Task3.isVowel2('a');
-		Task3.isVowel5('e');
-		Task3.moodSensor();
+		System.out.println(Task3.moodSensor());
 		Task3.fintTheNextDate(30, 07, 1996);
 	}
 	
-	static void print (Object msg) {
-		System.out.println(msg);
-	}
 	
-	static void isTriangle(double x1, double x2, double x3, double y1, double y2, double y3) {
-		boolean isTriangle = false;
-		boolean isRectangular = false;
+	
+	public static boolean isTriangle(double x1, double x2, double x3, double y1, double y2, double y3) {
+		
 		if ((x1!=x2) | (x2 !=x3) | (x1!=x3)) {
 			if ((y1 != y2) | (y2 != y3) | (y1!=y3)) {
-				isTriangle = true;
-				double a = 0;
-				double b = 0;
-				double c = 0;
-				a = Math.sqrt((x2 - x1) * (x2 - x1) - (y2 - y1) * (y2 - y1));
-				b = Math.sqrt((x3 - x1) * (x3 - x1) - (y3 - y1) * (y3 - y1));
-				c = Math.sqrt((x3 - x2) * (x3 - x2) - (y3 - y2) * (y3 - y2));
-				if((a*a == b*b + c*c) | (b*b == a*a + c*c) | (c*c == a*a + b*b)) {
-					isRectangular = true;
-				} 
+				return true;
 			} 
-		} 
-		Task3.print("IsTriangle? - " + isTriangle);
-		Task3.print("IsRectangular? - " + isRectangular);
+		}
+		return false; 
+		
 	}
 	
-	static int calcHeads (int age) {
+	public static boolean isRectangular(double x1, double x2, double x3, double y1, double y2, double y3) {
+		if(Task3.isRectangular(x1, x2, x3, y1, y2, y3)) {
+			
+			double a = Math.sqrt((x2 - x1) * (x2 - x1) - (y2 - y1) * (y2 - y1));
+			double b = Math.sqrt((x3 - x1) * (x3 - x1) - (y3 - y1) * (y3 - y1));
+			double c = Math.sqrt((x3 - x2) * (x3 - x2) - (y3 - y2) * (y3 - y2));
+			
+			if((a*a == b*b + c*c) | (b*b == a*a + c*c) | (c*c == a*a + b*b)) {
+				 return true;
+			} 
+		}
+		return false;
+	}
+	
+	final static int YOUNG_AGE = 200;
+	final static int OLD_AGE = 300;
+	final static int HEADS_IN_YOUNG_AGE = 3;
+	final static int HEADS_IN_OLD_AGE = 2;
+	final static int EYES = 2;
+	
+	public static int calcHeads (int age) {
 		int heads = 3;
-		final int YOUNG_AGE = 200;
-		final int OLD_AGE = 300;
-		
 		if(age < YOUNG_AGE) {
-			heads =age*3;
+			heads =age*HEADS_IN_YOUNG_AGE;
 		}else if(age<OLD_AGE) {
-			heads = 3 * YOUNG_AGE + 2 * (age - YOUNG_AGE);
+			heads = HEADS_IN_YOUNG_AGE * YOUNG_AGE + HEADS_IN_OLD_AGE * age - HEADS_IN_OLD_AGE *YOUNG_AGE;
 		}else {
-			heads = 3 * YOUNG_AGE + 2 *
-					(OLD_AGE - YOUNG_AGE) + 
-					(age - OLD_AGE);
+			heads = HEADS_IN_YOUNG_AGE * YOUNG_AGE + HEADS_IN_OLD_AGE * OLD_AGE
+					- HEADS_IN_OLD_AGE * YOUNG_AGE + 
+					age - OLD_AGE;
 		}
 		return heads;
-		
 	}
 	
-	static int calcEyes (int age) {
-		int eyes = 6;
-		final int YOUNG_AGE = 200;
-		final int OLD_AGE = 300;
-		
-		if(age <200) {
-			eyes = age*6;
-		}else if(age<300) {
-			eyes = 6 * YOUNG_AGE + 4 * (age - YOUNG_AGE);
-		}else {
-			eyes = 6 * YOUNG_AGE + 4 *
-					(OLD_AGE - YOUNG_AGE) + 
-					2*(age - OLD_AGE);
-		}
-		return eyes;
+	public static int calcEyes (int age) {
+		return Task3.calcHeads(age)*EYES;
 	}
 
 	
 	
 	
-	static boolean isVowel1 (char ch) {
-		boolean result = false;
+	public static boolean isVowel1 (char ch) {
+		
 		switch (ch) {
 		case 'a':
 		case 'e':
 		case 'i':
 		case 'o':
 		case 'u':
-			result = true;
-			break;
+			return true;
 		}
-		return result;
+		return false;
 	}
 	
-	static boolean isVowel2 (char ch) {
-		boolean result = false;
-		while ((ch=='a')||(ch =='e')||(ch=='i')||(ch=='o')||(ch=='u')) {
-			result=true;
-			break;
-		}
-		return result;
+	public static boolean isVowel2 (char ch) {
+		return (ch=='a')||(ch!='e')||(ch!='i')||(ch!='o')||(ch!='u');
 	}
 	
-	static boolean isVowel3 (char ch) {
-		boolean result = false;
-		if((ch=='a')||(ch!='e')||(ch!='i')||(ch!='o')||(ch!='u')) {
-			result = true;
-		}
-		Task3.print(result);
-		return result;
-	}
-	
-	static boolean isVowel4 (char ch) {
-		boolean result = false;
-		String s = (ch +"").toLowerCase();
-		if(s.contains("a")||s.contains("e")||s.contains("i")||s.contains("o")||s.contains("u")) {
-			result = true;
-		}
-		return result;
-	}
-	
-	static boolean isVowel5 (char ch) {
-		boolean result = false;
-		String s = (ch +"").toLowerCase();
-		if(s.equals("a")||s.equals("e")||s.equals("i")||s.equals("o")||s.equals("u")) {
-			result = true;
-		}
-		return result;
+	public static boolean isVowel3 (char ch) {
+		ch = Character.toLowerCase(ch);
+		return "aoeiy".contains(ch+"");
 	}
 	
 	
 	
-	static String moodSensor() {
-		String mood = ":(";
-		int QUANTITY_OF_MOODS = 4;
-		
+	final static int QUANTITY_OF_MOODS = 4;
+	final static String UPSET = ":(";
+	final static String HAPPY = ":)";
+	final static String WINK = ";)";
+	final static String NEUTRAL = ":|";
+	
+	public static String moodSensor() {
 		Random rand = new Random();
 		int randomNum = rand.nextInt(QUANTITY_OF_MOODS);
-		
 		switch (randomNum) {
 			case 0:
-				mood = ":)";
-				break;
+				return UPSET;
 			case 1:
-				mood = ":|";
-				break;
+				return HAPPY;
 			case 2:
-				mood = ";)";
-				break;
+				return WINK;
 		}
-		
-		Task3.print(mood);
-		return mood;
+		return NEUTRAL;
 	}
 	
-	static String fintTheNextDate(int day, int month, int year) {
+	
+	final static int LAST_DAY30 = 30;
+	final static int LAST_DAY28 = 28;
+	
+	public static String fintTheNextDate(int day, int month, int year) {
 		int newDay = day + 1;
-		final int LAST_DAY30 = 30;
-		final int LAST_DAY28 = 28;
+		
 		if (month == 4 || month == 6 || month == 9 || month == 11) {
 			if(day==LAST_DAY30) {
 				newDay = 1;
@@ -201,14 +166,12 @@ public class Task3 {
 			if (day > LAST_DAY30) {
 				newDay = 1;
 				month +=1;
-				
 			}
-			
 		}
 		String s = newDay + "." + month + "." + year;
-		Task3.print(s);
 		return s;
 	}
 	
 	
 }
+

01a558fce95e5906ae9c1299afc8c8743cece425
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-22 16:24:00 +0300

    Task 5 (updated)

--- MinAndMax.java
+++ MinAndMax.java
@@ -3,22 +3,33 @@ package massives;
 public class MinAndMax {
 	
 	public static int findMin (int [] arr) {
-		int min = arr [0];
-		for(int i = 0; i <arr.length; i++) {
-			if(arr[i] < min) {
-				min = arr[i];
+		int minIndex = -1;
+		if (arr.length > 0) {
+			int min = arr [0];
+			minIndex = 0;
+			for(int i = 1; i < arr.length; i++) {
+				if(arr[i] < min) {
+					min = arr[i];
+					minIndex = i;
+				}
 			}
 		}
-		return min;
+		return minIndex;
 	}
 	
+	
 	public static int findMax (int [] arr) {
-		int max = arr [0];
-		for(int i = 0; i <arr.length; i++) {
-			if(arr[i] > max) {
-				max = arr[i];
+		int maxIndex = -1; 
+		if (arr.length > 0) {
+			int max = arr [0];
+			maxIndex = 0;
+			for(int i = 1; i <arr.length; i++) {
+				if(arr[i] > max) {
+					max = arr[i];
+					maxIndex = i;
+				}
 			}
 		}
-		return max;
+		return maxIndex;
 	}
 }

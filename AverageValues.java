f3b596ec501f8918a07bf3d69a6e0724e78b074c
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-22 20:38:45 +0300

    Task 6

--- AverageValues.java
+++ AverageValues.java
@@ -4,25 +4,25 @@ public class AverageValues {
 public static double findAverageArithmetical (double [][] arr) {
 		
 		double sum = 0;
-		int quantityOfelements = 0;
+		int quantityOfElements = 0;
 		for(int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
 				sum += arr[i][j];
-				quantityOfelements++;
+				quantityOfElements++;
 			}
 		}
-		return sum / quantityOfelements;
+		return sum / quantityOfElements;
 	}
 	
 	static double findAverageGeomethrical (double[][] arr) {
 		double mult = 1;
-		int quantityOfelements = 0;
+		int quantityOfElements = 0;
 		for(int i = 0; i < arr[i].length; i++) {
 			for (int j = 0; j < arr.length; j++) {
 				mult *= arr[i][j];
-				quantityOfelements++;
+				quantityOfElements++;
 			}
 		}
-		return Math.pow(mult, 1/quantityOfelements);
+		return Math.pow(mult, 1/quantityOfElements);
 	}
 }

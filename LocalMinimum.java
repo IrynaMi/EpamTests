74ad0c69e1210ce801475dd12a0f6cd525668288
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-22 22:08:16 +0300

    Task 6

--- LocalMinimum.java
+++ LocalMinimum.java
@@ -1,13 +1,16 @@
 package task6;
 
 public class LocalMinimum {
-	public static int[] findPositionOfMinimum (double[][] arr) {
+	public static int[] findPositionOfLocalMinimum (double[][] arr) {
 		int []position = {-1, -1};
-		for(int i = 0; i<arr.length; i++) {
-			for (int j = 1; j < arr[i].length-1; j++) {
-				if(arr[i][j] < arr[i][j-1] & arr[i][j] < arr[i][j+1]) {
-					position[0] = i;
-					position[1] = j;
+		if (arr.length > 0 && arr[0].length > 2) {
+			for(int i = 0; i < arr.length; i++) {
+				int iter = arr[i].length-1;
+				for (int j = 1; j < iter; j++) {
+					if(arr[i][j] < arr[i][j-1] && arr[i][j] < arr[i][j+1]) {
+						position[0] = i;
+						position[1] = j;
+					}
 				}
 			}
 		}

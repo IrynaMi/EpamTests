b7c42e4f099e8a7b65c32ceb6f928d48eb13434b
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-22 16:00:19 +0300

    Task 5 (updated)

--- Averages.java
+++ Averages.java
@@ -4,7 +4,7 @@ public class Averages {
 	
 	public static double averageArithmetical (int [] arr) {
 		
-		int sum = 1;
+		double sum = 0;
 		for(int i = 0; i < arr.length; i++) {
 			sum += arr[i];
 		}

783234057977686fa15f421ef2b69667af2b03dd
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-23 22:03:00 +0300

    Update AircraftsParkController.java

--- task7/by/epam/preTraining/irina/tasks/task7/controller/AircraftsParkController.java
+++ task7/by/epam/preTraining/irina/tasks/task7/controller/AircraftsParkController.java
@@ -7,20 +7,8 @@ public class AircraftsParkController {
 
 	public static void main(String[] args) {
 		
-		AircraftPark park1 = new AircraftPark();
-		
-		Aircraft craft1 = new Aircraft(12345, 1800, 230, "Boenig 737");
-		Aircraft[] allCrafts = park1.addCraftToPark(craft1);
-		
-		Aircraft craft2 = new Aircraft(12345, 900, 230, "Boenig 777");
-		allCrafts = park1.addCraftToPark(craft2);
-	
-		park1.showAllCrafts(allCrafts);
-		System.out.println(park1.numOfCrafts);
-		
-		
-		Aircraft[] newCr = park1.addCraftsToPark(allCrafts);
-		System.out.println(newCr.length);
+		Creator creator = new Creator();
+		creator.createPark();
 	}
 
 }

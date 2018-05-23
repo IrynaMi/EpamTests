722207f7aeaf6900ff3b519b3bf1062a5ca39b2d
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-23 22:21:21 +0300

    Update Viewer.java

--- task7/by/epam/preTraining/irina/tasks/task7/view/Viewer.java
+++ task7/by/epam/preTraining/irina/tasks/task7/view/Viewer.java
@@ -1,17 +1,17 @@
 package by.epam.preTraining.irina.tasks.task7.view;
 
 import by.epam.preTraining.irina.tasks.task7.model.Aircraft;
+import by.epam.preTraining.irina.tasks.task7.model.AircraftPark;
 
 public class Viewer {
-public static void showAllCrafts (Aircraft[] allCrafts) {
-		
+	
+	public static void showAllCrafts(AircraftPark park) {
+		Aircraft [] allCrafts = park.getCrafts();
 		for (int i = 0; i < allCrafts.length; i++) {
-			if(allCrafts[i]!= null) {
-      
-				System.out.println("Model: " + allCrafts[i].getModel() + " || Price: " + allCrafts[i].getPrice() + " || Max Speed: " 
-			+ allCrafts[i].getMaxSpeed());
-      
-			} 
+			if (allCrafts[i] != null) {
+				System.out.println("Model: " + allCrafts[i].getModel() + " || Price: " + allCrafts[i].getPrice()
+						+ " || Max Speed: " + allCrafts[i].getMaxSpeed());
+			}
 		}
 	}
 

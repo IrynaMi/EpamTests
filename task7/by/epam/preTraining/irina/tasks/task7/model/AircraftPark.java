695c374bc722aad7d85d4cac9745244ef4f98fd6
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-26 17:13:21 +0300

    Task 7 and Task 8

--- task7/by/epam/preTraining/irina/tasks/task7/model/AircraftPark.java
+++ task7/by/epam/preTraining/irina/tasks/task7/model/AircraftPark.java
@@ -1,10 +1,16 @@
 package by.epam.preTraining.irina.tasks.task7.model;
 
 public class AircraftPark {
-	public int numOfCrafts;
-	public Aircraft[] crafts = new Aircraft[1];
-	public Aircraft[] newCrafts;
+	private int numOfCrafts = 0;
+	private Aircraft[] crafts;
+	private int peakOftack;
 	
+	
+	public AircraftPark(int size) {
+		crafts = new Aircraft[size];
+		peakOftack = -1;
+	}
+
 	public int getNumOfCrafts() {
 		return numOfCrafts;
 	}
@@ -20,86 +26,74 @@ public class AircraftPark {
 	public void setCrafts(Aircraft[] crafts) {
 		this.crafts = crafts;
 	}
-
-	public Aircraft[] getNewCrafts() {
-		return newCrafts;
-	}
-
-	public void setNewCrafts(Aircraft[] newCrafts) {
-		this.newCrafts = newCrafts;
-	}
-
-	public Aircraft[] addCraftToPark(Aircraft craft){
-		
-		for (int i = 0; i < crafts.length; i++) {
-			if(crafts[i] == null) {
-				crafts[i] = craft;
-				numOfCrafts++;
-				return crafts;
-			} 
+	
+	//check if Park is empty
+		public boolean isParkEmpty() {
+		return peakOftack == crafts.length - 1;
 		}
-		
-		if (numOfCrafts == crafts.length) {
-			newCrafts = new Aircraft[crafts.length*2];
+
+	// push element to container
+	public void addCraftToPark(Aircraft craft) {
+		Aircraft[] newCrafts;
+
+		if (isParkEmpty()) {
+			newCrafts = new Aircraft[crafts.length * 2];
 			System.arraycopy(crafts, 0, newCrafts, 0, crafts.length);
-			newCrafts[crafts.length] = craft;
+			newCrafts[++peakOftack] = craft;
+			numOfCrafts++;
+			crafts = newCrafts;
+		} else {
+			crafts[++peakOftack] = craft;
 			numOfCrafts++;
 		}
-		return crafts = newCrafts;
+
 	}
 	
-	public Aircraft[] addCraftsToPark(Aircraft[] newCrafts){
+
+	public Aircraft[] addCraftsToPark(Aircraft[] newCrafts) {
 		Aircraft[] newCraftPark = new Aircraft[crafts.length + newCrafts.length];
 		System.arraycopy(crafts, 0, newCraftPark, 0, crafts.length);
 		System.arraycopy(newCrafts, 0, newCraftPark, crafts.length, newCrafts.length);
 		return newCraftPark;
-		
+
 	}
 	
-	public int numberOfAircrafts (Aircraft[] craftPark) {
-		for (int i = 0; i < craftPark.length; i++) {
-			numOfCrafts++;
-		}
-		return numOfCrafts;
-	}
-	
-	public boolean isParkEmpty(Aircraft[] craftPark) {
-		for (int i = 0; i < craftPark.length; i++) {
-			numOfCrafts++;
-		}
-		return numOfCrafts==0;
-	}
-	
+
 	public Aircraft getCraftById(Aircraft[] craftPark, int id) {
+		if (!isParkEmpty()) {
+			return craftPark[id];
+		}
+		return null;
+	}
+
+	public Aircraft deleteCraftById(Aircraft[] craftPark, int id) {
+		if (!isParkEmpty()) {
+		craftPark[id] = null;
+		}
 		return craftPark[id];
 	}
-	
-	public Aircraft deleteCraftById(Aircraft[] craftPark, int id) {
-		return craftPark[id] = null;
-	}
-	
-	public Aircraft[] deleteAllCraftsFromPark(Aircraft[] craftPark) {
+
+	public void deleteAllCraftsFromPark(Aircraft[] craftPark) {
 		for (int i = 0; i < craftPark.length; i++) {
 			craftPark[i] = null;
 		}
-		
-		return craftPark;
 	}
-	
-	public Aircraft[] deleteCraftFromPark(Aircraft[] crafts, Aircraft craft){
-		for (int i = 0; i < crafts.length; i++) {
-			if(crafts[i].equals(craft)) {
-				crafts[i] = null;
+
+	public Aircraft[] deleteCraftFromPark(AircraftPark park, Aircraft craft) {
+		Aircraft[] crafts = park.getCrafts();
+		
+		if (!isParkEmpty()) {
+			for (int i = 0; i < crafts.length; i++) {
+				if (crafts[i].equals(craft)) {
+					crafts[i] = null;
+				}
 			}
 		}
-		
 		return crafts;
-		
 	}
-	
-	
-	
-	public Aircraft findTheFastestCraft (Aircraft[] allCrafts) {
+
+	public Aircraft findTheFastestCraft(Aircraft[] allCrafts) {
+		 
 		Aircraft theFasterstCraft = allCrafts[0];
 		for (int i = 1; i < allCrafts.length; i++) {
 			if (allCrafts[i].getMaxSpeed() > theFasterstCraft.getMaxSpeed()) {
@@ -107,6 +101,49 @@ public class AircraftPark {
 			}
 		}
 		return theFasterstCraft;
-		
 	}
+	
+	
+	
+	//delete last element in Stack
+	public Aircraft[] deleteLastInAircraft(Aircraft[] crafts) {
+		int topOfStack = crafts.length-1;
+		if(!isParkEmpty()) {
+			for (int i = topOfStack; i >= 0; i--) {
+				if (crafts[i] != null) {
+					crafts[i] = null;
+					numOfCrafts--;
+					break;
+				}
+			}
+		}
+		return crafts;
+	}
+	
+	//delete first element in Queue and shift all elements to the beginning
+	public Aircraft[] deleteFirstInAircraft(AircraftPark park) {
+		Aircraft [] crafts = park.getCrafts();
+		int start = 0;
+		
+		int elementsInQueue = park.getNumOfCrafts();
+		
+			if(crafts[start] != null) {
+				crafts[start] = null;
+				elementsInQueue--;
+			}
+		if(!isParkEmpty()) {	
+			for (int i = 0; i <= elementsInQueue; i++) {
+				for (int j = i+1; j <= elementsInQueue;) {
+					if(crafts[i]==null) {
+						crafts[i] = crafts[j];
+						crafts[j] = null;
+					}
+					break;
+				}
+			}
+		}
+		return crafts;
+	}
+	
+
 }

193863238ca1ec7f2fa146bef81218898c44e6a8
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-27 16:31:53 +0300

    Update AircraftsParkController.java

--- task7/by/epam/preTraining/irina/tasks/task7/controller/AircraftsParkController.java
+++ task7/by/epam/preTraining/irina/tasks/task7/controller/AircraftsParkController.java
@@ -1,8 +1,6 @@
 package by.epam.preTraining.irina.tasks.task7.controller;
 
-import by.epam.preTraining.irina.tasks.task7.model.Aircraft;
-import by.epam.preTraining.irina.tasks.task7.model.AircraftPark;
-
+import by.epam.preTraining.irina.tasks.task7.model.VehiclesPark;
 import by.epam.preTraining.irina.tasks.task7.view.Viewer;
 import util.Creator;
 
@@ -10,15 +8,15 @@ public class AircraftsParkController {
 
 	public static void main(String[] args) {
 		Creator creator = new Creator();
-		AircraftPark park = creator.createPark();
+		VehiclesPark park = creator.createPark();
 		Viewer.showAllCrafts(park);
 		System.out.println(park.getNumOfCrafts());
 		System.out.println("After taken last craft: ");
-		park.deleteLastInAircraft(park.getCrafts());
+		park.deleteLastInVehicle(park.getCrafts());
 		Viewer.showAllCrafts(park);
 		System.out.println(park.getNumOfCrafts());
 		System.out.println("After taken first craft: ");
-		park.deleteFirstInAircraft(park);
+		park.deleteFirstInVehicle(park);
 		Viewer.showAllCrafts(park);
 		System.out.println("The fastest craft in the park: ");
 		Viewer.showCraft(park.findTheFastestCraft());

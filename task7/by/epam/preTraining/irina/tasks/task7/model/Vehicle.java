4d46fb1ddf8dbee2182a8cc1c2ae7970f70eb5f4
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-31 16:22:26 +0300

    Update Vehicle.java

--- task7/by/epam/preTraining/irina/tasks/task7/model/Vehicle.java
+++ task7/by/epam/preTraining/irina/tasks/task7/model/Vehicle.java
@@ -1,6 +1,6 @@
 package by.epam.preTraining.irina.tasks.task7.model;
 
-public abcrtact class Vehicle {
+public abstract class Vehicle {
 	private double price;
 	private double maxSpeed;
 	private int numberOfPassangers;
@@ -44,13 +44,37 @@ public abcrtact class Vehicle {
 	public void setNumberOfPassangers(int numberOfPassangers) {
 		this.numberOfPassangers = numberOfPassangers;
 	}
-	
-	
 
 	public abstract void move(Vehicle v);
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(maxSpeed);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + numberOfPassangers;
+		temp = Double.doubleToLongBits(price);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
 
-	
-	
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vehicle other = (Vehicle) obj;
+		if (Double.doubleToLongBits(maxSpeed) != Double.doubleToLongBits(other.maxSpeed))
+			return false;
+		if (numberOfPassangers != other.numberOfPassangers)
+			return false;
+		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
+			return false;
+		return true;
+	}
 }

385c99c8a4b4f6fd2561c62b7b1af0e796e5b1df
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-31 16:23:53 +0300

    Update Creator.java

--- task7/by/epam/preTraining/irina/tasks/task7/util/Creator.java
+++ task7/by/epam/preTraining/irina/tasks/task7/util/Creator.java
@@ -1,24 +1,26 @@
-package by.epam.preTraining.irina.tasks.task7.util;
-
+package util;
 import by.epam.preTraining.irina.tasks.task7.model.Aircraft;
 import by.epam.preTraining.irina.tasks.task7.model.VehiclesPark;
 
 public class Creator {
-	VehiclesPark park1 = new VehiclesPark(2);
+	VehiclesPark park = new VehiclesPark(2);
 	
-	Aircraft craft1 = new Aircraft(12345, 1100, 230, "Як-130");
-	Aircraft craft2 = new Aircraft(12345, 900, 230, "Boenig 777");
-	Aircraft craft3 = new Aircraft(120_456_990, 600, 150, "TU-134");
-	Aircraft craft4 = new Aircraft(30_543_345, 770, 160, "TU-154");
-	Aircraft craft5 = new Aircraft(43543, 700, 160, "TU-154");
+	Aircraft craft1 = new Aircraft(1800, 1100, 230, "Як-130");
+	Aircraft craft2 = new Aircraft(2300, 900, 230, "Boenig 777");
+	Aircraft craft3 = new Aircraft(1000, 600, 150, "TU-134");
+	Aircraft craft4 = new Aircraft(560, 770, 160, "TU-154");
+	Aircraft craft5 = new Aircraft(400, 700, 160, "TU-154");
 	
 	public VehiclesPark createPark() {
-		park1.addCraftToPark(craft1);
-		park1.addCraftToPark(craft2);
-		park1.addCraftToPark(craft3);
-		park1.addCraftToPark(craft4);
-		park1.addCraftToPark(craft5);
-		return park1;
+		park.addVehicleToPark(craft1);
+		park.addVehicleToPark(craft2);
+		park.addVehicleToPark(craft3);
+		park.addVehicleToPark(craft4);
+		park.addVehicleToPark(craft5);
+		return park;
 	}
+	
+	
+	
 
 }

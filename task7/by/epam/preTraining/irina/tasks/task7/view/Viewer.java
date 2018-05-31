030a61ae9c53140f7c2ed9ce19a46327d15d4f3e
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-05-31 16:18:56 +0300

    Update Viewer.java

--- task7/by/epam/preTraining/irina/tasks/task7/view/Viewer.java
+++ task7/by/epam/preTraining/irina/tasks/task7/view/Viewer.java
@@ -1,33 +1,19 @@
 package by.epam.preTraining.irina.tasks.task7.view;
 
-import by.epam.preTraining.irina.tasks.task7.model.Aircraft;
 import by.epam.preTraining.irina.tasks.task7.model.Vehicle;
 import by.epam.preTraining.irina.tasks.task7.model.VehiclesPark;
 
 public class Viewer {
 	
 	public static void showAllCrafts(VehiclesPark park) {
-		Vehicle [] allCrafts = park.getVehicles();
-		if(park.getNumOfCrafts() !=0) {
-		for (int i = 0; i < allCrafts.length; i++) {
-			if (allCrafts[i] != null) {
-				System.out.println("Model: " + ((Aircraft) allCrafts[i]).getModel() + " || Price: " + allCrafts[i].getPrice()
-						+ " || Max Speed: " + allCrafts[i].getMaxSpeed());
-			}
-		}
-		} else {
-			System.out.println("Park is empty!");
-		}
+		System.out.println(park);
 	}
-
 	
 	public static void showCraft (Vehicle vehicle) {
-		if(vehicle != null) {
-			System.out.println("Model: " + ((Aircraft) vehicle).getModel() + " || Price: " + vehicle.getPrice() + " || Max Speed: " + 
-					vehicle.getMaxSpeed() + " || Number of passengers: " + vehicle.getNumberOfPassangers());
-		} else {
-			System.out.println("Aircraft not found!");
-		}
+		System.out.println(vehicle);
 	}
-
+	
+	public static void showNumOfVehicles(VehiclesPark park) {
+		System.out.println("Total amount of vehicles: " + park.getNumOfCrafts() + "\n");
+	}
 }

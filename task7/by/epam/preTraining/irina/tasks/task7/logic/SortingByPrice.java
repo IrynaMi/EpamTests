e737fed6c30bf63cc090e13b2a2361b5c5971285
Iryna <38079441+IrynaMi@users.noreply.github.com>
2018-06-04 12:10:12 +0300

    Task 9

--- task7/by/epam/preTraining/irina/tasks/task7/logic/SortingByPrice.java
+++ task7/by/epam/preTraining/irina/tasks/task7/logic/SortingByPrice.java
@@ -6,6 +6,8 @@ import by.epam.preTraining.irina.tasks.task7.model.VehiclesPark;
 import util.CompareByPrice;
 
 public class SortingByPrice {
+	
+	//sorted with Comparator
 	public void sortByPrice(VehiclesPark park) {
 		Arrays.sort(park.getVehicles(), new CompareByPrice());
 	}
